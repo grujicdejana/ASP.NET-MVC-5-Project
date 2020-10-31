16da91c5433ff2769b4a461b7536f8be1d759af4
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2020-10-31 02:06:29 +0100

    Working with Data

---
+++ VidlyProject/VidlyProject/Controllers/CustomersController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using VidlyProject.Models;
+
+namespace VidlyProject.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        [HttpGet]
+        public ActionResult Index()
+        {
+            /*List<Customer> customers = GetCustomers();*/
+
+            //na ovaj nacin preuzimamo sve customer iz baze
+
+            
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList(); //include membership type kako bi se prikazalo na webu, jer je membership type referencirajuca klasa
+            return View(customers);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int idCustomer)
+        {
+
+            /*var customer = GetCustomerById(idCustomer);
+
+            if(customer.Name != null)
+            {
+                return View(customer);
+            }
+            else
+            {
+                return HttpNotFound();
+            }*/
+
+            
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == idCustomer);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        /*private Customer GetCustomerById(int id)
+        {
+            List<Customer> customers = GetCustomers();
+
+            var customer = new Customer();
+
+            foreach(var cust in customers)
+            {
+                if (cust.Id == id)
+                {
+                    customer = cust;
+                    break;
+                }
+            }
+
+            return customer;
+        }*/
+
+        /*private List<Customer> GetCustomers()
+        {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer {Id = 1, Name = "John Smith"},
+                new Customer {Id = 2, Name = "Mary Williams"}
+            };
+
+            return customers;
+        }*/
+    }
+}
---
+++ VidlyProject/VidlyProject/Controllers/MoviesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using VidlyProject.Models;
+using VidlyProject.ViewModels;
+
+namespace VidlyProject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult GetMovies()
+        {
+            /*var movies = new List<Movie>
+            {
+                new Movie {Name = "Shrek"},
+                new Movie {Name = "Wall-e"}
+            };*/
+
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
+
+            return View(movies);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int idMovie)
+        {
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == idMovie);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        /*public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }*/
+    }
+}
