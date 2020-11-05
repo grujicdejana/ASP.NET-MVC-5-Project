9a14b3e48385f9560a219858db1a050ed93e1780
dejanagrujic <39495830+dejanagrujic@users.noreply.github.com>
2020-11-05 14:29:15 +0100

    Build an API for CRUD operations - Movie

--- VidlyProject/VidlyProject/App_Start/MappingProfile.cs
+++ VidlyProject/VidlyProject/App_Start/MappingProfile.cs
@@ -13,7 +13,10 @@ namespace VidlyProject.App_Start
         public MappingProfile()
         {
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            //ovo je uradjeno zbog metode update, da prilikom apdejtovanja ignorise Id, on se ne mjenja
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(m => m.Id, opt => opt.Ignore());
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
         }
     }
 }
